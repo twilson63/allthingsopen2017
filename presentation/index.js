d6e1818b51314c6cb5f7b67953f649eed386e003
Tom Wilson <tom@jackhq.com>
2017-10-22 19:26:42 -0400

    update slideshow

--- presentation/index.js
+++ presentation/index.js
@@ -1,8 +1,9 @@
 // Import React
-import React from 'react'
+import React from "react"
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
   Deck,
@@ -14,295 +15,322 @@ import {
   Text,
   MarkdownSlides,
   Link,
-  Image
-} from 'spectacle'
+  Image,
+  Code,
+  CodePane
+} from "spectacle"
 
 // Import image preloader util
-import preloader from 'spectacle/lib/utils/preloader'
+import preloader from "spectacle/lib/utils/preloader"
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default'
+import createTheme from "spectacle/lib/themes/default"
 
 // Require CSS
-require('normalize.css')
-require('spectacle/lib/themes/default/index.css')
+require("normalize.css")
+require("spectacle/lib/themes/default/index.css")
 
 const images = {
-  now: require('../assets/white-bg-logo-1200.png'),
-  jrs: require('../assets/jrs-code-logo.jpg'),
-  logo: require('../assets/formidable-logo.svg'),
-  markdown: require('../assets/markdown.png')
+  now: require("../assets/white-bg-logo-1200.png"),
+  jrs: require("../assets/jrs-code-logo.jpg"),
+  logo: require("../assets/formidable-logo.svg"),
+  twentyone: require("../assets/twenty-one.png"),
+  markdown: require("../assets/markdown.png")
 }
 
 preloader(images)
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quartenary: '#CECECE'
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quartenary: "#CECECE"
   },
-  { primary: 'Montserrat', secondary: 'Helvetica' }
+  { primary: "Montserrat", secondary: "Helvetica" }
 )
 
 export default class Presentation extends React.Component {
-  render () {
+  render() {
     return (
       <Deck
-        transition={[ 'zoom', 'slide' ]}
+        transition={["zoom", "slide"]}
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={[ 'zoom' ]} bgColor='primary'>
-          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
-            Fun Functional Programming
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Fun Functional JavaScript
           </Heading>
-          <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
-            Three functional modules that can make full stack functional programming fun!
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            W/REACT
           </Text>
-          <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
-            SYNTAXCON, Charleston, SC, May 19, 2017
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            AllThingsOpen, Raliegh, NC, Oct 23, 2017
           </Text>
         </Slide>
-        <Slide transition={[ 'fade' ]} bgColor='tertiary'>
-          <Heading size={6} textColor='primary' caps>Who Am I?</Heading>
-          <Heading size={1} textColor='secondary'>Tom Wilson</Heading>
-          <Heading size={5} textColor='secondary'>
-            Chief Technology Innovation Officer
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Who Am I?
           </Heading>
-          <Heading size={4} textColor='secondary'>
-            Tabula Rasa HealthCare, Inc
+          <Heading size={1} textColor="secondary">
+            Tom Wilson
           </Heading>
-          <Heading size={5} textColor='secondary'>
-            JRS Innovation Center
+
+          <Heading size={5} textColor="secondary">
+            @twilson63
           </Heading>
-          <Heading size={5} textColor='secondary'>JRS Coding School</Heading>
-          <Text size={6} textColor='secondary' />
+          <Text size={6} textColor="secondary" />
         </Slide>
-        {MarkdownSlides`
-![JRS Code](${images.jrs})
-
-12 Week Immersive Training
-
-Jan - May - Sept
-
----
-
-> Shared Mutable state is the root of all evil... Pete Hunt Facebook
-
----
-
-### Why is shared mutable state bad?
-
-* It is easy to create bugs
-* It is hard to maintain
-
----
-
-### Why?
-
-* Less Bugs
-* Easier to Maintain
-* JavaScript supports FP
-
----
-
-### Stack
-
-* React/Redux
-* Tachyons
-* Ramda
-* NodeJS
-* CouchDB/MySQL
-* Now
-
----
-
-### Talking About
-
-* Tachyons
-* Ramda
-* Now
-
----
-
-# Tachyons
-
----
-
-### What is it?
-
-Tachyons is like sub atomic classes that can be combined together to create components.
-
-
-`}
-        <Slide transition={[ 'fade' ]} bgColor='primary'>
-          <Link
-            target='_new'
-            href='http://codepen.io/twilson63/pres/vmJwxZ?editors=0010'
-          >
-            <Text>Demo</Text>
-          </Link>
-        </Slide>
-        {MarkdownSlides`
-
-### Why?
-
-* Design Systems break as they scale
-* Systems like Tachyons et al. approach this problem by instead documenting and limiting *properties* of components. (I like to think of this as “subatomic” design.)
-* Those subatomic components can be combined in thousands of ways to create hundreds of components.
-
----
-
-### Tachyons
-
-* Less context switching
-* Functional and Composable
-* Do the math!
-          `}
-        {MarkdownSlides`
-### How to learn tachyons?
-
-* http://tachyons.io
-* https://tachyons.pro/
-* https://atom.io/packages/atom-tachyons
-
----
-
-![Ramda Logo](http://ramda.jcphillipps.com/logo/ramdaFilled_200x235.png)
-
-# Ramda
-
----
-
-### What is Ramda?
-
-Ramda is a functional javascript utility that takes functions first and
-every function is auto curried.
-
----
-
-Ramda enables separation of functional logic from your presentation logic in
-a very testable way.
-
-
-        `}
-        <Slide transition={[ 'fade' ]} bgColor='primary'>
-          <Link
-            target='_new'
-            href='https://runkit.com/twilson63/590c7d1967f45a00121c2b6a'
-          >
-            <Text>Demo</Text>
-          </Link>
-        </Slide>
-        {MarkdownSlides`
-### How to learn ramdajs?
-
-* http://ramdajs.com/
-* [What Function Should I Use](https://github.com/ramda/ramda/wiki/What-Function-Should-I-Use%3F)
-* [Try Ramda](http://ramdajs.com/repl/?v=0.23.0)
-
-
-        `}
         <Slide>
-          <Image src={images.now} margin='0px auto 40px' height='293px' />
-          <Heading size={6} textColor='secondaryπ' caps>NowJS</Heading>
+          <Image src={images.jrs} />
+          <Text>12 Week Immersive Training</Text>
+          <Text>Jan | May | Sept</Text>
         </Slide>
-        {MarkdownSlides`
-### What is NowJS?
-
-A more functional way of thinking about deployments! All deployments are immutable!
-
----
-
-### Why NowJS? (One Line)
-
-* One line to buy a domain
-* One line to deploy your code
-* One line to alias your app url
-
----
-
-# Demo
-
----
-
-now
-
----
-
-now alias [url] [alias]
-
----
-
-now domains buy foo.xyz
-
----
-
-Make a change
-
----
-
-now alias [url] [alias]
-
----
-
-### There is more
-
-* now secrets
-* now ls
-* now logs
-
----
-
-### Recap
-
-* React/Redux
-* NodeJS/ExpressJS
-* CouchDB/MySQL
-
-Are strong components in your full-stack!
-
----
-
-### Fun
-
-* Tachyons
-* Ramda
-* Now
-
----
-
-# Thank You
-
-### Questions
-
-        `}
-        <Slide transition={[ 'fade' ]} bgColor='tertiary'>
-          <Heading size={6} textColor='primary' caps>Typography</Heading>
-          <Heading size={1} textColor='secondary'>Heading 1</Heading>
-          <Heading size={2} textColor='secondary'>Heading 2</Heading>
-          <Heading size={3} textColor='secondary'>Heading 3</Heading>
-          <Heading size={4} textColor='secondary'>Heading 4</Heading>
-          <Heading size={5} textColor='secondary'>Heading 5</Heading>
-          <Text size={6} textColor='secondary'>Standard text</Text>
+        <Slide>
+          <Heading size={1} textColor="tertiary">
+            What is Functional JavaScript?
+          </Heading>
         </Slide>
-        <Slide transition={[ 'fade' ]} bgColor='primary' textColor='tertiary'>
-          <Heading size={6} textColor='secondary' caps>Standard List</Heading>
+        <Slide>
+          <Image height={400} src={images.twentyone} />
+          <Text>{"21.now.sh"}</Text>
+        </Slide>
+        <Slide>
+          <Appear>
+            <Image
+              height={200}
+              src="https://arcweb.co/wp-content/uploads/2016/10/react-logo-1000-transparent.png"
+            />
+          </Appear>
+          <Appear>
+            <Image
+              height={200}
+              src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png"
+            />
+          </Appear>
+          <hr />
+          <Appear>
+            <Image
+              height={200}
+              src="http://ramda.jcphillipps.com/logo/ramdaFilled_200x235.png"
+            />
+          </Appear>
+          <Appear>
+            <Image
+              height={200}
+              src="https://opencollective-production.s3-us-west-1.amazonaws.com/5e1b02e0-bce5-11e6-9ca1-bb6c7a9a84e0.png"
+            />
+          </Appear>
+          <Appear>
+            <Image
+              height={200}
+              src="https://pbs.twimg.com/profile_images/784360755319697408/cw6zhtGK.jpg"
+            />
+          </Appear>
+        </Slide>
+        <Slide>
+          <Text textColor="tertiary">Characteristics of FP JS</Text>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <Appear>
+              <ListItem>Declarative</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Immutable</ListItem>
+            </Appear>
+            <Appear>
+              <ListItem>Composable</ListItem>
+            </Appear>
           </List>
         </Slide>
-        <Slide transition={[ 'fade' ]} bgColor='secondary' textColor='primary'>
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide>
+          <Heading size={1} textColor="tertiary">
+            Declarative
+          </Heading>
+          <Appear>
+            <Text>
+              <span style={{ textDecoration: "line-through" }}>
+                Describe How?
+              </span>
+            </Text>
+          </Appear>
+          <Appear>
+            <Text>Describe What!</Text>
+          </Appear>
         </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1em" }}
+            lang="js"
+            source={require("raw-loader!../assets/router.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1.1em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/home.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/player.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".8em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/dealer.example")}
+          />
+        </Slide>
+        <Slide>
+          <Heading size={1} textColor="tertiary">
+            Immutable
+          </Heading>
+          <Appear>
+            <Text>
+              <span style={{ textDecoration: "line-through" }}>Update</span>
+            </Text>
+          </Appear>
+          <Appear>
+            <Text>Replace</Text>
+          </Appear>
+        </Slide>
+        <Slide>
+          <Image
+            height={500}
+            src="https://media.licdn.com/mpr/mpr/shrinknp_800_800/AAEAAQAAAAAAAAc3AAAAJGEzNjRmMWZmLWI3YTgtNGNmYy1hOTU0LTQ0ZDU4ZjgwMDJiZQ.jpg"
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".8em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/react-index.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".8em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/redux.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".6em" }}
+            lang="jsx"
+            source={require("raw-loader!../assets/newgame.example")}
+          />
+        </Slide>
+        <Slide>
+          <Image
+            height={400}
+            src="https://hptechblogs.com/content/images/2017/08/2674118914543364345a1efea35608aa.gif"
+          />
+        </Slide>
+        <Slide>
+          <Heading size={1} textColor="tertiary">
+            Composable
+          </Heading>
+          <Appear>
+            <Text>
+              <span style={{ textDecoration: "line-through" }}>
+                Inheritance or Mixin
+              </span>
+            </Text>
+          </Appear>
+          <Appear>
+            <Text>Compose or Combine</Text>
+          </Appear>
+        </Slide>
+
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".9em" }}
+            lang="js"
+            source={require("raw-loader!../assets/reducers.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: ".9em" }}
+            lang="js"
+            source={require("raw-loader!../assets/layout.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1em" }}
+            lang="js"
+            source={require("raw-loader!../assets/home.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1em" }}
+            lang="js"
+            source={require("raw-loader!../assets/button.example")}
+          />
+        </Slide>
+        <Slide>
+          <CodePane
+            style={{ fontSize: "1em" }}
+            lang="js"
+            source={require("raw-loader!../assets/getScore.example")}
+          />
+        </Slide>
+        <Slide>
+          <Image
+            height={200}
+            src="https://arcweb.co/wp-content/uploads/2016/10/react-logo-1000-transparent.png"
+          />
+          <Image
+            height={200}
+            src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png"
+          />
+          <hr />
+          <Image
+            height={200}
+            src="http://ramda.jcphillipps.com/logo/ramdaFilled_200x235.png"
+          />
+          <Image
+            height={200}
+            src="https://opencollective-production.s3-us-west-1.amazonaws.com/5e1b02e0-bce5-11e6-9ca1-bb6c7a9a84e0.png"
+          />
+          <Image
+            height={200}
+            src="https://pbs.twimg.com/profile_images/784360755319697408/cw6zhtGK.jpg"
+          />
+        </Slide>
+        <Slide>
+          <Heading>Challenge</Heading>
+          <Text>Lets calculate the players winning percentage</Text>
+          <Text>And allow the player to tweet it</Text>
+        </Slide>
+        <Slide>
+          <Text>Live Code</Text>
+        </Slide>
+        <Slide>
+          <Heading>Thank You</Heading>
+        </Slide>
+        <Slide>
+          <Heading>Resources</Heading>
+          <List>
+            <ListItem>
+              <a href="https://21.now.sh">Game</a>
+            </ListItem>
+            <ListItem>
+              <a href="https://github.com/jrs-innovation-center/twenty-one">
+                Source Code
+              </a>
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide />
       </Deck>
     )
   }
